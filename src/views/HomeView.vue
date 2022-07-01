e8c05652d5b75f689a8b9ed55e2786f69478f3ea
clarimi <mirandolaclarice@gmail.com>
2022-07-02 00:18:40 +0200

    Aggiunti pannelli con animazioni di prova

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,6 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 </script>
 <template>
   <main>
-
-
     <header>
       <a href="/" id="site-logo">
         <img src="./../assets/c_logo.svg" />
@@ -18,35 +16,135 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
       </div>
     </header>
 
+    <div class="titolo-hard-skills-falso">
+      <div>
+        <h1>
+          <span class="hard">Hard</span>
+          <span class="skills">Skills&nbsp;</span>
+          <span class="and">and&nbsp;</span>
+          <span class="soft">&nbsp;Soft</span>
+          <span class="ware">ware</span>
+        </h1>
+      </div>
+    </div>
     <div class="container">
-      <div class="panel" id="first-panel">
+      <div class="panel" id="copertina-panel">
         <div class="panel-content">
           <img id="hero-logo" src="./../assets/title.svg" />
           <span id="hero-scroll-text">Scroll to projects</span>
         </div>
       </div>
-      <section class="panel" id="second-panel">
+      <div class="panel" id="descrizione-panel">
+        <div class="panel-content">
+          <img id="person" src="./../assets/person.png" />
+          <h1>A few words about me</h1>
+          <p>Hi I’m Clarice e mo ti becchi sto pezzo di paragrafo di minchiate about me, consectetur adipiscing elit ut
+            aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non
+            enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim
+            diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis eu volutpat
+            odio facilisis mauris sit amet massa vitae tortor condimentum lacinia quis vel eros donec ac odio tempor
+            orci dapibus ultrices in iaculis nunc sed augue lacus, viverra vitae congue eu, consequat ac felis donec et
+            odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus</p>
+        </div>
+      </div>
+      <section class="panel panel-falso">
+        <div class="panel-content">
+          Pannello falzo
+        </div>
+      </section>
+      <section class="panel" id="skill-panel">
         <div class="panel-content">
           <div class="skill-list-container">
-            <h1>HardSkills and Software</h1>
-            <ol class="skill-list">
-              <li>
-                <h2>Marketing</h2>
-                <p>Social media marketing ∙ Blockchain and NFT ∙ Content creation ∙ Advertising ∙ Google Analytics</p>
-              </li>
-              <li>
-                <h2>Digital Design</h2>
-                <p>Art Direction ∙ UX and UI</p>
-              </li>
-              <li>
-                <h2>Visual Design</h2>
-                <p>Illustration ∙ Motion Graphic ∙ Editing and Video making ∙ Sound design</p>
-              </li>
-              <li>
-                <h2>Creative Developer</h2>
-                <p>Html ∙ Javascript ∙ CSS ∙ Wordpress ∙ Gatsby ∙ React.js ∙ Vue.js</p>
-              </li>
-            </ol>
+            <h1>
+              <span class="hard">Hard</span>
+              <span class="skills">Skills&nbsp;</span>
+              <span class="and">and&nbsp;</span>
+              <span class="soft">&nbsp;Soft</span>
+              <span class="ware">ware</span>
+            </h1>
+            <div class="skill-software-list">
+              <ol class="skill-list">
+                <li>
+                  <h2>Marketing</h2>
+                  <p>Social media marketing ∙ Blockchain and NFT ∙ Content creation ∙ Advertising ∙ Google Analytics</p>
+                </li>
+                <li>
+                  <h2>Digital Design</h2>
+                  <p>Art Direction ∙ UX and UI</p>
+                </li>
+                <li>
+                  <h2>Visual Design</h2>
+                  <p>Illustration ∙ Motion Graphic ∙ Editing and Video making ∙ Sound design</p>
+                </li>
+                <li>
+                  <h2>Creative Developer</h2>
+                  <p>Html ∙ Javascript ∙ CSS ∙ Wordpress ∙ Gatsby ∙ React.js ∙ Vue.js</p>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="panel" id="software-panel">
+        <div class="panel-content">
+          <div class="skill-list-container">
+            <h1>
+              <span class="hard">Hard</span>
+              <span class="skills">Skills&nbsp;</span>
+              <span class="and">and&nbsp;</span>
+              <span class="soft">&nbsp;Soft</span>
+              <span class="ware">ware</span>
+            </h1>
+            <div class="skill-software-list">
+              <ol class="software-list">
+                <li>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                </li>
+                <li>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                </li>
+                <li>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                </li>
+                <li style="opacity: 0; height: ">
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                  <div class="software">
+                    <img src="./../assets/icone-software/after.png" />After effects
+                  </div>
+                </li>
+
+
+              </ol>
+            </div>
           </div>
         </div>
       </section>
@@ -62,7 +160,41 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
       </section>
       <section class="panel" id="fifth-panel">
         <div class="panel-content">
-          FEMO QUALCOSSA ASSIEME
+          <div class="contact-container">
+
+            <div class="three-lines">
+              <div class="line-one">
+                <h1 class="line-one-text">
+                  <span class="letsmake"> Let's make&nbsp;&nbsp;</span>
+                  <span class="something">&nbsp;something&nbsp;&nbsp;</span>
+                  <span class="great">&nbsp;great!</span>
+
+                </h1>
+              </div>
+
+              <div class="line-two">
+                <a href="mailto:mirandolaclarice@gmail.com" class="button-collabora">KEEP IN TOUCH</a>
+                <h2>
+                  <span class="mail">mirandolaclarice@gmail.com</span>
+                </h2>
+              </div>
+
+              <div class="line-three">
+                <h3 class="for-line-collaborations">
+                  <span class="for">for&nbsp;&nbsp;</span>
+                  <img id="line-divider" src="./../assets/line.svg" />
+
+                  <span class="collaborations">&nbsp;&nbsp;collaborations.</span>
+                </h3>
+                <img id="star" src="./../assets/star.svg" />
+              </div>
+            </div>
+
+            <div>
+              <img id="contacts-img" src="./../assets/contacts.svg" />
+            </div>
+
+          </div>
         </div>
       </section>
     </div>
@@ -78,7 +210,7 @@ export default {
   mounted() {
     gsap.registerPlugin(ScrollTrigger);
 
-    let scrollTween = gsap.to(".panel:not(:last-child):not(:nth-child(3))", {
+    let scrollTween = gsap.to(".panel:not(:last-child)", {
       xPercent: -100,
       ease: "none",
       stagger: 0.5,
@@ -86,14 +218,14 @@ export default {
         trigger: ".container",
         start: "top top",
         end: "+=4000",
-        markers: true,
+        //markers: true,
         scrub: true,
         pin: true,
         id: "animazione-panel"
       }
     });
 
-    gsap.to(".panel:nth-child(3)", {
+    gsap.to(".dsada:nth-child(3)", {
       xPercent: -100,
       ease: "none",
       stagger: 0.5,
@@ -101,7 +233,7 @@ export default {
         trigger: ".container",
         start: (n) => { console.log(n); return 4000; },
         end: (n) => { console.log(n); return 6000; },
-        markers: true,
+        // markers: true,
         scrub: true,
         pin: true,
         id: "animazione-panel"
@@ -112,20 +244,81 @@ export default {
 
     for (var i = 0; i < liItems; i++) {
 
-      gsap.from(`#second-panel ol > li:nth-child(${i + 1})`, {
-        x: 800 + (500 * i),
+      gsap.from(`#skill-panel ol > li:nth-child(${i + 1})`, {
+        x: 800 + (400 * i),
         scrollTrigger: {
-          trigger: "#first-panel",
+          trigger: "#descrizione-panel",
           containerAnimation: scrollTween,
-          start: "right right",
-          end: "center 20%",
+          start: "right 82%",
+          end: "center 10%",
           scrub: true,
-          // markers: true,
+          //markers: true,
           toggleActions: "play none none reset",
-          id: "voce-1",
+          id: "skill-1",
         }
       });
     }
+
+
+    for (var i = 0; i < 3; i++) {
+
+      gsap.from(`#software-panel ol > li:nth-child(${i + 1})`, {
+        x: 800 + (400 * i),
+        scrollTrigger: {
+          trigger: "#skill-panel",
+          containerAnimation: scrollTween,
+          start: "right 60%",
+          end: "right center",
+          scrub: true,
+          //markers: true,
+          toggleActions: "play none none reset",
+          id: "sw-1",
+        }
+      });
+    }
+
+    gsap.to("#skill-panel h1", {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#skill-panel",
+        containerAnimation: scrollTween,
+        start: "right 68%",
+        end: "right 65%",
+        scrub: true,
+        // markers: true,
+        toggleActions: "play none none reset",
+        id: "title-1",
+      }
+    });
+    gsap.set(".titolo-hard-skills-falso", { opacity: 0 });
+
+    gsap.to(".titolo-hard-skills-falso", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: "#skill-panel",
+        containerAnimation: scrollTween,
+        start: "right 68%",
+        end: "right 67%",
+        scrub: true,
+        // markers: true,
+        toggleActions: "play none reverse reset",
+        id: "title-1",
+      }
+    });
+
+    gsap.to(".titolo-hard-skills-falso", {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: "#skill-panel",
+        containerAnimation: scrollTween,
+        start: "right 48%",
+        end: "right 45%",
+        scrub: true,
+        markers: true,
+        toggleActions: "play none reverse reset",
+        id: "title-nascondi-1",
+      }
+    });
   }
 }
 
